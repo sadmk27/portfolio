9b81df197155b4b43265c396007c9d578d52c30e
sadmk27 <83779736+sadmk27@users.noreply.github.com>
2025-03-31 19:24:58 +0200

    add card component

--- flutter_portfolio/lib/pages/projects_page.dart
+++ flutter_portfolio/lib/pages/projects_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/components/card.dart';
 import 'pages_layout.dart';
 
 class ProjectsPage extends StatelessWidget {
@@ -6,14 +7,47 @@ class ProjectsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(
-      currentRoute: '/projects',
-      child: const Center(
-        child: Text(
-          'ProjectPage!',
-          style: TextStyle(color: Colors.white, fontSize: 24),
+    final screenWidth = MediaQuery.of(context).size.width;
+    final isMobile = screenWidth < 600;
+
+    if (isMobile) {
+      return MainLayout(
+        currentRoute: '/projects',
+        child: Column(
+          children: List.generate(3, (index) {
+            return Flexible(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AppCard(
+                  imageUrl: 'https://via.placeholder.com/150',
+                  textContent: 'Karta ${index + 1}',
+                ),
+              ),
+            );
+          }),
         ),
-      ),
-    );
+      );
+    } else {
+      return MainLayout(
+        currentRoute: '/projects',
+          child: GridView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: 9,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 12,
+            crossAxisSpacing: 12,
+            childAspectRatio: 3 / 4,
+          ),
+          itemBuilder: (context, index) {
+            return AppCard(
+              imageUrl: 'https://via.placeholder.com/150',
+              textContent: 'Karta ${index + 1}',
+            );
+          },
+        ),
+      );
+    }
   }
 }
